7ae6033faa2023025a202208fd92a09dbe133cd6
Khavkin <hdm@ukr.net>
2023-05-07 23:02:34 +0300

    styled Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,48 @@
-import { AppBar, Box, Link, Toolbar } from '@mui/material';
+import { AppBar, Avatar, Box, Toolbar, Typography, Link } from '@mui/material';
 import { NavLink } from 'react-router-dom';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import { theme } from '../../Theme/Theme';
 
 export const Header = () => {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Box component={'nav'}>
-          <Link component={NavLink} to="/">
-            Logo
+      <Toolbar component={'nav'}>
+        <Link component={NavLink} to="/">
+          <Avatar sx={{ color: theme.palette.primary.main }}>
+            <SubscriptionsIcon />
+          </Avatar>
+        </Link>
+        <Box sx={{ display: 'flex', flexDirection: 'row', gap: '30px', ml: '30px' }}>
+          <Link
+            component={NavLink}
+            to="/"
+            underline="none"
+            color="inherit"
+            sx={{
+              '&.active': {
+                color: theme.palette.secondary.main,
+              },
+            }}
+          >
+            <Typography textAlign="center" sx={{ textTransform: 'uppercase' }}>
+              Home
+            </Typography>
           </Link>
-          <Link component={NavLink} to="/">
-            Home
-          </Link>
-          <Link component={NavLink} to="/tweets">
-            Tweets
+
+          <Link
+            component={NavLink}
+            to="/tweets"
+            underline="none"
+            color="inherit"
+            sx={{
+              '&.active': {
+                color: theme.palette.secondary.main,
+              },
+            }}
+          >
+            <Typography textAlign="center" sx={{ textTransform: 'uppercase' }}>
+              Tweets
+            </Typography>
           </Link>
         </Box>
       </Toolbar>
